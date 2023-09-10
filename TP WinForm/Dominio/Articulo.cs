4f61c384560aff6caed7cdd47190cce323692642
unknown <palmierifacundo@gmail.com>
2023-09-10 10:33:39 -0300

    Creación proyecto Dominio y Winform-app

---
+++ TP WinForm/Dominio/Articulo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Articulo
+    {
+        public string CodigoArticulo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int Marca { get; set; }
+        public int Categoria { get; set; }
+        public string Imagen { get; set; }
+        public decimal Precio { get; set; }
+    }
+}
